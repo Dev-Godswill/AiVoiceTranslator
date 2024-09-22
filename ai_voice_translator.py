8dcb649a0a0640b004dad11591089ac658f665dd
Dev-Godswill <daudugodswill2019@gmail.com>
2024-09-22 21:14:08 +0100

    new changes

--- ai_voice_translator.py
+++ ai_voice_translator.py
@@ -1,23 +1,57 @@
 import gradio as gr
+import assemblyai as ai
+from translate import Translator
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
 
-def voice_to_voice():
+def voice_to_voice(audio_file):
+
+    #transcribe audio
+    transcription_response= audio_transcription(audio_file)
+
+    if transcription_response.status == aai.Transcriptions.error:
+        raise gr.Error(transcription_response.error)
+    else:
+        text = transcription_response.txt
+
+    es_translation, tr_translation= text_translation(text)
 
 
-def audio_transcription():
-    return True
+
+def audio_transcription(audio_file):
+
+    aai.settings.api_key = ""
+
+    transcriber = aai.Transcriber()
+    transcription = transcriber.transcribe(audio_file)
+
+    return transcription
 
 
 
 def text_transaltion():
-    return True
+    
+    translator_es = Translator(from_lang="en", to_lang="es")
+    es_text=translator_es.translate(text)
+
+    translator_tr = Translator(from_lang="en", to_lang="tr")
+    tr_text=translator_tr.translate(text)
+
+    return es_text, tr_text
 
 
 def text_to_speech():
     return True
 
+
+audio_input= gr.Audio(
+    sources=["microphone"],
+    type="filepath"
+)
+
 demo = gr.Interface(
     fn=voice_to_voice,
-    inputs=,
+    inputs=audio_input,
     outputs=[gr.Audio(label="Spanish"), gr.Audio(label="Turkish"), gr.Audio(label="Japanese")]
 )
 
